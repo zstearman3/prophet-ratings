0790e3821b3ce3e7b7e8f7d87ec466565aa777d1
Zach Stearman <zstearman3@gmail.com>
2024-02-08 19:26:46 -0600

    Add javascripts

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values  = { state: Boolean }
+  static targets = [ "menu", "x", "bars" ]
+
+  toggle() {
+    this.stateValue = !this.stateValue
+
+    if (this.stateValue) {
+      this.openMenu()
+      this.showX()
+    } else {
+      this.closeMenu()
+      this.showBars()
+    }
+  }
+
+  openMenu() {
+    this.menuTarget.classList.remove("hidden");
+  }
+
+  closeMenu() {
+    this.menuTarget.classList.add("hidden");
+  }
+
+  showBars() {
+    this.xTarget.classList.add("hidden")
+    this.barsTarget.classList.remove("hidden")
+  }
+
+  showX() {
+    this.xTarget.classList.remove("hidden")
+    this.barsTarget.classList.add("hidden")
+  }
+}
