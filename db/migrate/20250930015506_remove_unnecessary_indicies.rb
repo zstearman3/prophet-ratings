61bcffb56216ad00b190c59bf1c618ae23277c3c
Zach Stearman <zstearman@hey.com>
2025-09-29 21:08:02 -0500

    Fix migration issues

--- db/migrate/20250930015506_remove_unnecessary_indicies.rb
+++ db/migrate/20250930015506_remove_unnecessary_indicies.rb
@@ -1,4 +1,4 @@
-class RemoveUnneccessaryIndecies < ActiveRecord::Migration[7.1]
+class RemoveUnnecessaryIndicies < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def up
@@ -13,6 +13,6 @@ class RemoveUnneccessaryIndecies < ActiveRecord::Migration[7.1]
               %i[team_id season_id snapshot_date],
               name: :idx_on_team_id_season_id_snapshot_date_8de7607130,
               algorithm: :concurrently
-    add_index :game_odds, name: :index_game_odds_on_game_id, algorithm: :concurrently
+    add_index :game_odds, :game_id, name: :index_game_odds_on_game_id, algorithm: :concurrently
   end
 end
